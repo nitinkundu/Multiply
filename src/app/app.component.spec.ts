cd39be5076bac93e39e69b7561e802375e68147a
nitinkundu <50735799+nitinkundu@users.noreply.github.com>
2019-07-12 17:16:17 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,21 +13,21 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  /*it('should create the app', () => {
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });*/
+  }));
 
-  it(`should have as title 'Multiply'`, () => {
+  it(`should have as title 'Multiply'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Multiply');
-  });
+  }));
 
-  /*it('should render title in a h1 tag', () => {
+  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Multiply!');
-  });*/
+    expect(compiled.querySelector('h1').textContent).toContain('!Multiplication Made Easy!');
+  }));
