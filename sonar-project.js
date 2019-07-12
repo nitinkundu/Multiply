0c3120446cd5638870e8bede98ab02f097e66fb5
nitinkundu <50735799+nitinkundu@users.noreply.github.com>
2019-07-12 16:29:04 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,8 +1,8 @@
-const sonarqubeScanner = require('sonarqube-scanner');
-     sonarqubeScanner({
-       serverUrl: 'http://3.14.251.87:9000',
-       options : {
-       'sonar.sources': '.',
-       'sonar.inclusions' : 'src/**' // Entry point of your code
-       }
-     }, () => {});
+sonar.projectKey=Nitin_Angular
+sonar.projectName=Multiply
+sonar.projectVersion=1.0
+sonar.sources=src
+sonar.host.url=http://3.14.251.87:9000
+sonar.language=ts
+sonar.tests=src/app
+sonar.python.coverage.reportPaths=sonarcoverage.xml
