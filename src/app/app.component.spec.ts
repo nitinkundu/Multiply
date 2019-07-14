17575a0dfd9cdac3d992e43ae0b0dba17f12af4f
nitinkundu <50735799+nitinkundu@users.noreply.github.com>
2019-07-14 18:36:17 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,6 +29,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('!Multiplication Made Easy!');
+    expect(compiled.querySelector('h1').textContent).toContain('Multiplication Made Easy');
   });
 })
