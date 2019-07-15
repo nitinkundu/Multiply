44be01ce3b9e5d20de4c18c0b94c61c10733ea11
nitinkundu <nitinkundu111.nk@gmail.com>
2019-07-15 14:54:55 +0530

    update jenkinsfile

--- appserver.js
+++ appserver.js
@@ -1,10 +1,5 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Hello Everyone");
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8090, function(){
+    console.log('Server running on 8090...');
 });
-
-
-server.listen(3000, '127.0.0.1');
-console.log("Listening to port 3000");
