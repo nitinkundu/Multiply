45ad5955c16281c33b6e4cca5ba8c1460ba18741
nitinkundu <nitinkundu111.nk@gmail.com>
2019-07-22 17:42:58 +0530

    Nothing Important

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,26 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Multiply';
+  title = 'Calculator';
   num1: number;
   num2: number;
   result: number;
-  
+  add() {
+      this.result = this.num1 + this.num2;
+  }
+  substract() {
+      this.result = this.num1 - this.num2;
+  }
   multiply() {
       this.result = this.num1 * this.num2;
+  }
+  divide() {
+      this.result = this.num1 / this.num2;
+  }
+  square() {
+    this.result = this.num1 * this.num1;
+  }
+  cube() {
+  this.result = this.num1 * this.num1 * this.num1;
   }
 }
